9d154d0786b693807f933aa4260da3653b5d4f7f
nguyenvanthinh <nguyenvanthinh@f88.vn>
2020-02-14 16:56:36 +0700

    a

---
+++ mssql/mssql_client.js
@@ -0,0 +1,111 @@
+﻿let mssql_client = function mssql_client() {
+    const REMOTE_SERVER_GRPC = "0.0.0.0:5001";
+
+    //#region [ VARIABLE ]
+
+    const $ = this;
+
+    const _UUID = require("uuid");
+    const _JOB = require('cron').CronJob;
+     
+    const _GRPC = require("grpc");
+    const _PROTO_LOADER = require("@grpc/proto-loader");
+
+    let _INITED = false;
+    let _CONNECTED = false;
+    let _STATE = 'NONE';
+    
+    const _SESSION_ID = _UUID.v4();
+
+    let _CLIENT = null;
+    let _CHANNEL;
+    let _PROTO = null;
+
+    this.on_state = null;
+    this.on_send_callback = null;
+
+    //#endregion
+
+    const notify___on_message = (m) => {
+        $.___log_key('NOTIFY___ON_MESSAGE: m = ', m);
+        switch (m.type) {
+            case 'JOIN_NEW_CONNECT_OK':
+                _CONNECTED = true;
+                _STATE = 'CONNECT_OK';
+                setTimeout(function () { $.on_state(true, _STATE); });
+                break;
+            default:
+                if ($.on_send_callback) $.on_send_callback(null, m);
+                break;
+        }
+    };
+
+    const notify___on_error = (err) => {         
+        console.log('ERROR DISCONNECT TO SERVER ...');
+
+        if (_CONNECTED) {
+            _CONNECTED = false;
+            _STATE = 'SERVER_DISCONNECT';
+            setTimeout(function () { $.on_state(false, _STATE); });
+        }
+    };
+
+    const f_init = () => {
+        //Load the protobuf
+        _PROTO = _GRPC.loadPackageDefinition(
+            _PROTO_LOADER.loadSync("message.proto", {
+                keepCase: true,
+                longs: String,
+                enums: String,
+                defaults: true,
+                oneofs: true
+            })
+        );
+
+        //Create gRPC client
+        _CLIENT = new _PROTO.MessageEngine.MessageBroker(REMOTE_SERVER_GRPC, _GRPC.credentials.createInsecure());        
+    };
+
+    this.f_start = function (on_state_callback_, on_send_callback_) {
+        if (this.on_state == null) this.on_state = on_state_callback_;
+        if (this.on_send_callback == null) this.on_send_callback = on_send_callback_;
+
+        if (_INITED == false) {
+            _INITED = true;
+
+            if ($.___log == null) $.___log = (...agrs) => { }
+            if ($.___log_key == null) $.___log_key = (key, ...agrs) => { }
+            if ($.___log_error == null) $.___log_error = (key, ...agrs) => { }
+
+            f_init();
+        }
+
+        _CHANNEL = _CLIENT.streamMessage({});
+        _CHANNEL.on('error', notify___on_error);
+        _CHANNEL.on('data', notify___on_message);    
+        _CHANNEL.write({ id: _SESSION_ID, type: 'JOIN_NEW_CONNECT' });        
+    };
+
+    this.f_send_text = (text) => {
+        if (_CONNECTED)
+            _CHANNEL.write({ id: _SESSION_ID, text: text });
+        else
+            if ($.on_send_callback)
+                $.on_send_callback({ ok: false, message: 'Disconnect to ' + REMOTE_SERVER_GRPC });
+    };
+
+    new _JOB('*/2 * * * * *', function () {
+        if (_CONNECTED == false) {
+            //$.f_start();
+        }
+    }).start();
+     
+    //--------------------------------------------------------------------------------------------    
+};
+
+mssql_client.instance = null;
+mssql_client.getInstance = function () {
+    if (this.instance === null) this.instance = new mssql_client();
+    return this.instance;
+};
+module.exports = mssql_client.getInstance();
---
+++ mssql/mssql_server.js
@@ -0,0 +1,220 @@
+﻿
+const _JOB = require('cron').CronJob;
+var uuid = require("uuid");
+
+let grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+
+const APP_NAME = 'MSSQL_SERVER';
+
+//Read terminal Lines
+var readline = require("readline");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const server = new grpc.Server();
+const SERVER_ADDRESS = "0.0.0.0:5001";
+
+// Load protobuf
+let proto = grpc.loadPackageDefinition(
+    protoLoader.loadSync("message.proto", {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    })
+);
+
+const _MSG = [];
+const _USERS = {};
+const user___Disconect = (id) => {
+    if (_USERS[id]) delete _USERS[id];
+};
+function streamMessage(call) {
+    let session_id = null;
+
+    console.log('SERVER_STREAM_MESSAGE: ...');
+
+    call.on('data', (m) => {
+        console.log('SERVER_STREAM_MESSAGE: m = ', m);
+        if (m && m.id && m.id.length > 0 && m.type == 'JOIN_NEW_CONNECT') {
+            session_id = m.id;
+            _USERS[m.id] = call;
+            m.type = 'JOIN_NEW_CONNECT_OK';
+            call.write(m);
+        } else {
+            _MSG.push(m);
+        }
+    });
+
+    call.on('error', (err) => {
+        user___Disconect(session_id);
+        console.log('SERVER_STREAM_MESSAGE: ', session_id, ' error = ', err);
+    });
+
+    call.on('cancelled', () => {
+        user___Disconect(session_id);
+        console.log('SERVER_STREAM_MESSAGE -> CANCELLED: ', session_id,  call.cancelled, call.finished, call.reading);
+    });
+
+    call.on('end', () => {
+        user___Disconect(session_id);
+        console.log('SERVER_STREAM_MESSAGE -> END: ', session_id,  call.cancelled, call.finished, call.reading);
+        setImmediate(() => {
+            console.log('SERVER_STREAM_MESSAGE -> END -> REQUEST CANCELLED: ', session_id,  call.cancelled); // true
+        });
+    });
+}
+
+function removeUser(call, callback) {
+    const m = call.request;
+
+    let ok = false;
+    if (m.id && m.id.length > 0 && _USERS[m.id]) {
+        delete _USERS[m.id];
+        ok = true;
+    }
+
+    console.log('remove user: ' + m.id + ' -> ', Object.keys(_USERS));
+
+    callback(null, ok);
+}
+
+function joinUser(call, callback) {
+    const m = call.request;
+
+    let ok = false;
+    if (m.id && m.id.length > 0 && _USERS[m.text]) {
+        _IDS.push(m.id);
+        _USERS[m.id] = _USERS[m.text]
+        delete _USERS[m.text];
+        ok = true;
+    }
+
+    console.log('join: ' + m.text + ' -> ' + m.id, Object.keys(_USERS));
+
+    callback(null, ok);
+
+    //call.write({ sender: APP_NAME, id: id, type: 'CONNECT_OK' });
+    //call.write({ sender: 'CACHE', id: id, type: 'SET_SESSION_ID' });
+    //broadCast({ sender: 'CACHE', id: id, type: 'WELCOME_NEW_USER_JOINED' });
+}
+
+// Receive message from client
+function sendMessage(call, callback) {
+    const m = call.request;
+    console.log(m);
+    broadCast_not_ItSelf(m);
+}
+
+function getBuffer(call, callback) {
+    const m = call.request;
+    console.log(m);
+    callback(null, m);
+}
+
+function streamBuffer(call) {
+    const m = {};
+    call.on('data', (data) => {
+        console.log('server: got data', data);
+    });
+
+    call.on('error', (err) => {
+        console.log('server: got error', err);
+    });
+
+    call.on('cancelled', () => {
+        console.log('server: request cancelled', call.cancelled, call.finished, call.reading);
+    });
+
+    call.on('end', () => {
+        console.log('server: request ended', call.cancelled, call.finished, call.reading);
+
+        setImmediate(() => {
+            console.log('request cancelled', call.cancelled); // true
+        });
+    });
+}
+
+function messageReply(m) {
+    if (m && _USERS[m.id]) _USERS[m.id].write(m);
+}
+
+function broadCast_not_ItSelf(message, id_) {
+    const a = Object.keys(_USERS);
+    a.forEach(id => {
+        if (id != id_) _USERS[id].write(message);
+    });
+}
+
+function broadCast(message) {
+    Object.values(_USERS).forEach(user => {
+        user.write(message);
+    });
+}
+
+function app___Start() {
+
+    new _JOB('* * * * * *', function () {
+        if (_MSG.length > 0) {
+            const m = _MSG.shift();
+            m.ok = true;
+            messageReply(m);
+        }
+    }).start();
+
+    // Define server with the methods and start it
+    server.addService(proto.MessageEngine.MessageBroker.service, {
+        streamMessage: streamMessage,
+        joinUser: joinUser,
+        removeUser: removeUser,
+        sendMessage: sendMessage,
+        streamBuffer: streamBuffer,
+        getBuffer: getBuffer
+    });
+    server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
+    server.start();
+
+    rl.on("line", function (text) {
+        switch (text) {
+            case 'exit':
+                process.exit();
+                break;
+            case 'cls':
+                console.clear();
+                break;
+            default:
+                broadCast({ sender: 'CACHE', receiver: '*', text: text });
+                break;
+        }
+    });
+
+    //client.send({ id: ___id, sender: username, text: 'LOGIN' }, (err, m_) => { });
+}
+
+app___Start();
+
+//for client call sendBuffer
+////const grpc = require('grpc');
+////const PROTO_PATH = __dirname + '/depot.proto';
+////const protoDescriptor = grpc.load(PROTO_PATH);
+////const depot = protoDescriptor.depot;
+
+////const client = new depot.Depot('localhost:50051', grpc.credentials.createInsecure());
+
+////const call = client.streamBuffer((err, data) => {
+////    if (err) {
+////        console.log(err);
+////        return;
+////    }
+////    console.log('got data', data);
+////});
+
+////call.write({ id: '1', data: new Uint8Array([0]) });
+
+////setTimeout(() => {
+////    call.cancel(new Error('there was an error'));
+////}, 3000);
